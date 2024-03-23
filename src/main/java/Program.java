564dedb34b11f07ce20d716988b1ecdcfa752863
IMART0 <imartirosyan@vk.com>
2024-03-23 14:21:50 +0300

    Реализован класс Program

---
+++ src/main/java/Program.java
@@ -0,0 +1,12 @@
+public class Program {
+    private String channel;
+    private BroadcastsTime time;
+    private String name;
+
+    public Program(String channel, BroadcastsTime time, String name) {
+        this.channel = channel;
+        this.time = time;
+        this.name = name;
+    }
+
+}
