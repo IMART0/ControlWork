69fd14022c49e6d606613d273a12b0314dc9d668
IMART0 <imartirosyan@vk.com>
2024-03-23 15:11:38 +0300

    Дополнен Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner s = new Scanner(new File("src/main/java/schedule.txt"));
         List<Program> list = new ArrayList<Program>();
-        Map<String, List<Program>> program = new HashMap<>();
+        Map<String, List<Program>> programs = new HashMap<>();
         String read = s.nextLine();
         while (s.hasNextLine()){
             String channel = read;
@@ -20,9 +21,43 @@ public class Main {
                 if (s.hasNextLine())
                     read = s.nextLine();
             };
-            program.put(channel, channelList);
+            programs.put(channel, channelList);
         }
         s.close();
-        System.out.println(program);
+        Collections.sort(list, Comparator.comparing(program -> program.getTime()));
+        //7
+        BroadcastsTime currentTime = new BroadcastsTime((byte) 14, (byte) 30); // Пример текущего времени
+        for (Program program : list) {
+            if (program.getTime().equals(currentTime)) {
+                System.out.println(program.getName());
+            }
+        };
+
+        //8
+        String searchName = "Tatarstan today. Открытый миру";
+        List<Program> foundPrograms = list.stream()
+                .filter(program -> program.getName().equals(searchName))
+                .collect(Collectors.toList());
+        System.out.println(foundPrograms);
+
+        //9
+        BroadcastsTime currentTime1 = new BroadcastsTime((byte) 15, (byte) 20);
+        String channelName = "#НТВ";
+        List<Program> channelPrograms = programs.get(channelName);
+        if (channelPrograms != null) {
+            for (Program program : channelPrograms) {
+                if (program.getTime().equals(currentTime)) {
+                    System.out.println(program.getName());
+                }
+            }
+        }
+        //10
+        BroadcastsTime startTime = new BroadcastsTime((byte) 5, (byte) 0);
+        BroadcastsTime endTime = new BroadcastsTime((byte) 9, (byte) 0);
+        for (Program program : channelPrograms) {
+            if (program.getTime().between(startTime, endTime)) {
+                System.out.println(program.getName());
+            }
+        }
     }
 }
